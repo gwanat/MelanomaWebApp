aa817e3c66b4063cb17057b45c1bb533bd593354
Gabriela Wanat <gwanat876@gmail.com>
2024-04-12 12:52:08 +0200

    Add project files.

---
+++ MelanomaWebApp/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MelanomaWebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index_pl()
+        {
+            return View();
+        }
+    }
+}
---
+++ MelanomaWebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using MelanomaWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MelanomaWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string currentLanguage = HttpContext.Request.Query["language"];
+
+
+            if (string.IsNullOrEmpty(currentLanguage) || currentLanguage != "PL")
+            {
+                currentLanguage = "EN";
+            }
+
+            string viewName = "Index";
+            if (currentLanguage == "PL")
+            {
+                viewName += "_pl";
+            }
+
+            return View(viewName);
+        }
+
+        public IActionResult Index_pl()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
